c7ca2d8c6109683adb0e082c6189b5dc8bdcfc76
Russell <russhwolf@gmail.com>
2018-12-26 19:18:30 -0500

    Update versions and now everything works!

--- app/src/iosMain/kotlin/sample/httpClientEngine.kt
+++ app/src/iosMain/kotlin/sample/httpClientEngine.kt
@@ -1,7 +1,7 @@
 package sample
 
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.HttpClientEngineConfig
 import io.ktor.client.engine.ios.IosClientEngine
+import io.ktor.client.engine.ios.IosClientEngineConfig
 
-actual val httpClientEngine: HttpClientEngine = IosClientEngine(HttpClientEngineConfig())
+actual val httpClientEngine: HttpClientEngine = IosClientEngine(IosClientEngineConfig())
